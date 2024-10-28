9c3c300e6130ca5c1735db9f3781ea6c826c61d9
RonaldoFidelis <ronaldofidelis.ti@gmail.com>
2024-10-28 14:33:29 -0300

    Style: changed font color and added effect hover in button login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 
 function Login() {
   return (
-    <div className="flex">
+    <div className="flex bg-background">
       {/*Tela de login*/}
       <div className="w-2/3 h-screen flex flex-col items-center justify-center">
         <div className=" flex flex-col items-center gap-1 mb-14">
@@ -11,8 +11,8 @@ function Login() {
         {/*Inputs*/}
         <div className="flex flex-col gap-1">
           <div className="flex flex-col gap-5 items-center">
-            <input className="border-2 border-backgroundBlue w-[400px] h-[55px] rounded-[10px] p-4 outline-none font-extralight" placeholder="Digite seu E-mail" type="email" name="" id="" required />
-            <input className="border-2 border-backgroundBlue w-[400px] h-[55px] rounded-[10px] p-4 outline-none font-extralight" placeholder="Digite sua Senha" type="password" name="" id="" required />
+            <input className="bg-background border-2 border-backgroundBlue w-[400px] h-[55px] rounded-[10px] p-4 outline-none font-extralight" placeholder="Digite seu E-mail" type="email" name="" id="" required />
+            <input className="bg-background border-2 border-backgroundBlue w-[400px] h-[55px] rounded-[10px] p-4 outline-none font-extralight" placeholder="Digite sua Senha" type="password" name="" id="" required />
           </div>
           <div className="flex">
             <a className="ml-auto text-xs font-extralight transition hover:text-backgroundBlue duration-300" href="#">
@@ -22,7 +22,7 @@ function Login() {
         </div>
         {/*BTN*/}
         <div className="flex flex-col gap-1 mt-14 items-center">
-          <button className="bg-backgroundBlue text-white font-light rounded-[5px] w-[150px] h-[45px] p-2">Login</button>
+          <button className="bg-backgroundBlue text-background font-light rounded-[5px] w-[150px] h-[45px] p-2 transition ease-in-out hover:bg-opacity-80 duration-500">Login</button>
           <span className="font-extralight text-xs transition hover:text-backgroundBlue duration-300">
             <a href="#">Solicite cadastro</a>
           </span>
@@ -39,7 +39,7 @@ function Login() {
           </span>
 
           {/* Mensagem motivacional principal */}
-          <span className="text-white md:text-6xl lg:text-8xl font-semibold">
+          <span className="text-background md:text-6xl lg:text-8xl font-semibold">
             Juntos<br/>fazemos<br/>a diferença!
           </span>
 
