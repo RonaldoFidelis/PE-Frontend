a3ea2e7b39fbe0a4591f3aa8483f8e4157be4313
RonaldoFidelis <ronaldofidelis.ti@gmail.com>
2024-10-28 21:24:06 -0300

    Refactor: Adjust page responsiveness

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,16 +3,16 @@ function Login() {
   return (
     <div className="flex bg-background">
       {/*Tela de login*/}
-      <div className="w-2/3 h-screen flex flex-col items-center justify-center">
+      <div className="w-screen md:w-2/3 h-screen flex flex-col items-center justify-center">
         <div className=" flex flex-col items-center gap-1 mb-14">
           <h1 className="text-font text-2xl font-extralight">Consultorio comunitario</h1>
           <h2 className="text-font text-lg font-extralight">Faça seu login</h2>
         </div>
         {/*Inputs*/}
-        <div className="flex flex-col gap-1">
+        <div className=" flex flex-col gap-1">
           <div className="flex flex-col gap-5 items-center">
-            <input className="bg-background border-2 border-backgroundBlue w-[400px] h-[55px] rounded-[10px] p-4 outline-none font-extralight" placeholder="Digite seu E-mail" type="email" name="" id="" required />
-            <input className="bg-background border-2 border-backgroundBlue w-[400px] h-[55px] rounded-[10px] p-4 outline-none font-extralight" placeholder="Digite sua Senha" type="password" name="" id="" required />
+            <input className="w-[350px] bg-background border-2 border-backgroundBlue md:w-[400px] h-[55px] rounded-[10px] p-4 outline-none font-extralight" placeholder="Digite seu E-mail" type="email" name="" id="" required />
+            <input className="w-[350px] bg-background border-2 border-backgroundBlue md:w-[400px] h-[55px] rounded-[10px] p-4 outline-none font-extralight" placeholder="Digite sua Senha" type="password" name="" id="" required />
           </div>
           <div className="flex">
             <a className="ml-auto text-xs font-extralight transition hover:text-backgroundBlue duration-300" href="#">
@@ -29,9 +29,9 @@ function Login() {
         </div>
       </div>
       {/*Banner*/}
-      <div className="w-1/2 h-screen flex items-center justify-center bg-backgroundBlue bg-opacity-50">
+      <div className="hidden md:w-1/2 md:h-screen md:flex md:items-center md:justify-center bg-backgroundBlue bg-opacity-50">
         {/* Contêiner interno que centraliza o conteúdo verticalmente */}
-        <div className="h-max flex flex-col">
+        <div className="h-max flex flex-col justify-center">
 
           {/* Mensagem de boas-vindas leve */}
           <span className="text-font md:text-4xl lg:text-5xl font-extralight ">
