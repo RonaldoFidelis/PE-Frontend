d068e55fb0113b0732422530830afec618267499
RonaldoFidelis <ronaldofidelis.ti@gmail.com>
2024-10-29 15:27:52 -0300

    Style: centered banner text

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,7 @@ import birds from "../assets/birds.png";
 function Banner() {
   return (
     <section className="w-full h-[450px] mt-[73px] bg-backgroundBlue/60 flex">
-      <div className="md:w-2/3 md:h-full flex justify-center items-center">
+      <div className="w-full md:w-2/3 md:h-full flex justify-center items-center">
         <div className="p-5">
           <h3 className="text-backgroundSecondary text-sm font-light">Terapia gratuita</h3>
           <h1 className="text-background text-xl lg:text-2xl font-normal leading-6">Seu psicologo a qualquer<br />hora, em qualquer lugar.</h1>
